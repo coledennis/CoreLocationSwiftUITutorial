3e0569b69d47107aa1adc6d5587d02d2abc2c59b
Cole Dennis <coleedennis@gmail.com>
2022-09-20 20:51:38 -0700

    Initial Commit

---
+++ CoreLocationSwiftUITutorial/CoreLocationSwiftUITutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreLocationSwiftUITutorialApp.swift
+//  CoreLocationSwiftUITutorial
+//
+//  Created by Cole Dennis on 9/20/22.
+//
+
+import SwiftUI
+
+@main
+struct CoreLocationSwiftUITutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
