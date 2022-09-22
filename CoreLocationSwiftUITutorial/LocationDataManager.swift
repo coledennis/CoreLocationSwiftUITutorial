d7d093dbcfe583b88a69132ae54d7ed96289f408
Cole Dennis <coleedennis@gmail.com>
2022-09-21 20:58:09 -0700

    Set up LocationDataManager class

--- CoreLocationSwiftUITutorial/LocationDataManager.swift
+++ CoreLocationSwiftUITutorial/LocationDataManager.swift
@@ -9,11 +9,32 @@ import Foundation
 import CoreLocation
 
 class LocationDataManager : NSObject, CLLocationManagerDelegate {
-   var locationManager = CLLocationManager()
-
-   override init() {
-      super.init()
-      locationManager.delegate = self
-   }
-
+    var locationManager = CLLocationManager()
+    
+    override init() {
+        super.init()
+        locationManager.delegate = self
+    }
+    
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        switch manager.authorizationStatus {
+        case .authorizedWhenInUse:  // Location services are available.
+            // Insert code here of what should happen when Location services are authorized
+            locationManager.requestLocation()
+            break
+            
+        case .restricted, .denied:  // Location services currently unavailable.
+            // Insert code here of what should happen when Location services are NOT authorized
+            break
+            
+        case .notDetermined:        // Authorization not determined yet.
+            manager.requestWhenInUseAuthorization()
+            break
+            
+        default:
+            break
+        }
+    }
+    
+    
 }
